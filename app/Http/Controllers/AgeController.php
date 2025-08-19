e72c8b3ab823eec4ede4a2b786a9989f5d7b2157
idn-fahmifn <fahmifn.idn@gmail.com>
2025-08-19 10:19:01 +0700

    2.2

--- app/Http/Controllers/AgeController.php
+++ app/Http/Controllers/AgeController.php
@@ -20,6 +20,17 @@ class AgeController extends Controller
         $request->validate([
             'nama' => ['required', 'string', 'min:3', 'max:30'], 
             'umur' => ['required', 'numeric', 'min:1', 'max:100'], 
+        ],[
+            // error nama
+            'nama.required' => 'Nama harus lu isi',
+            'nama.string' => 'Nama harus berupa text',
+            'nama.min' => 'Nama minimal 3 karakter',
+            'nama.max' => 'Nama maksimal 30 karakter',
+            // error nama
+            'umur.required' => 'Umur harus lu isi',
+            'umur.numeric' => 'Umur harus berupa angka',
+            'umur.min' => 'Umur minimal 1 Tahun',
+            'umur.max' => 'Umur maksimal 100 Tahun',
         ]);
     }
 }
